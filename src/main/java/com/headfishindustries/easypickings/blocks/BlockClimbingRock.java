797ab9a94ca88166112b8adddb96df59b83119b8
TheIcyOne <icy.headfishindustries@gmail.com>
2018-07-08 03:40:29 +0100

    made climbing rock climbable
    this isn't half-assed at all

--- src/main/java/com/headfishindustries/easypickings/blocks/BlockClimbingRock.java
+++ src/main/java/com/headfishindustries/easypickings/blocks/BlockClimbingRock.java
@@ -6,12 +6,16 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockClimbingRock extends Block{
 
-	public BlockClimbingRock() {
+		public BlockClimbingRock() {
 		super(Material.ROCK);
 		this.setCreativeTab(CreativeTabs.DECORATIONS);
 		this.setUnlocalizedName("climbing_rock");
@@ -21,8 +25,8 @@ public class BlockClimbingRock extends Block{
 		}
 
 	
-	@Override
-	 public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+		@Override
+	 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
 		if (worldIn.getBlockState(pos.up()).isFullBlock()) return;
 		for (int x = -1; x<=1; x+=2) {
 				if (worldIn.getBlockState(pos.add(x, 0, 0)).isOpaqueCube() && worldIn.getBlockState(pos.add(x, 1, 0)).isOpaqueCube()) {
@@ -37,10 +41,23 @@ public class BlockClimbingRock extends Block{
 				worldIn.setBlockState(pos.up(), worldIn.getBlockState(pos));
 				worldIn.setBlockToAir(pos);
 				return;
+				}
 			}
 		
-	}
-	}
+		}
+		
+		@Override
+		 public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+	    {
+			return new AxisAlignedBB(0.01D, 0D, 0.01D, 0.99D, 1D, 0.99D);
+	    }
+		
+		@Override
+	    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity ent)
+	    {
+			ent.motionY+=0.03;
+			ent.fallDistance = 0;
+	    }
 	
 
 }
